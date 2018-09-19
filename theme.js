edfb0db6e35be4b7699764cf628594f4bc4e35e6
Ben Iofel <ben@flow.io>
2018-09-19 02:03:03 -0400

    more stuff

--- theme.js
+++ theme.js
@@ -1,13 +1,15 @@
 import { yellow } from 'mdx-deck/themes'
 import okaidia from 'react-syntax-highlighter/styles/prism/okaidia'
 import prismGraphql from 'react-syntax-highlighter/languages/prism/graphql'
+import prismJson from 'react-syntax-highlighter/languages/prism/json'
 
 export default {
   ...yellow,
   prism: {
     style: okaidia,
     languages: {
-        graphql: prismGraphql
+        graphql: prismGraphql,
+        json: prismJson
     }
   }
 }
